be10cc7fcb26870b2a15fceddf064052ce8fb55a
Kyllian AUGER <kyllianauger14@gmail.com>
2025-03-13 16:39:34 +0100

    commit #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
-
+#include <Wire.h>
+#include <SSD1306.h>
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Initialiser la communication série (facultatif pour le débogage)
+  Serial.begin(115200);
+SSD1306 ecranOled(0x3c, 5, 4);
+  // Initialiser l'écran OLED
+  ecranOled.init();
+  ecranOled.clear();
+
+  // Définir la police de caractères
+  ecranOled.setFont(ArialMT_Plain_16);
+
+  // Dessiner le texte (nom de l'application et version)
+  ecranOled.drawString(10, 10, "NomApp: MonApp");
+  ecranOled.drawString(10, 30, "Version: 1.0");
+
+  // Afficher sur l'écran
+  ecranOled.display();
 }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
+Serial.print("Vitesse de transmission :");
+Serial.println(115200);
+Serial.println();
+delay(3000);
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
-}
+}  
